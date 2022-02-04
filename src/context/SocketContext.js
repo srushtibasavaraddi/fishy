40b20f2202ebff24e3c750d8575e1b2e7dd17b14
Srushti Basavaraddi <sbasavaraddi@gamil.com>
2022-02-04 11:18:30 +0530

    aws amplify

--- src/context/SocketContext.js
+++ src/context/SocketContext.js
@@ -1,4 +1,4 @@
 import io from "socket.io-client";
 import React from "react";
-export const socket = io("https://fishy-equilibrium-madiee.herokuapp.com/");
+export const socket = io("https://host.dwv482omu6gd0.amplifyapp.com/");
 export const SocketContext = React.createContext();
